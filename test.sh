d678b1313b405b974d8d0c91db97b9d879ab0ce0
Qiwei Hu <97610834+JerryV77@users.noreply.github.com>
2024-08-04 12:38:59 -0700

    Delete test.sh

--- test.sh
+++
@@ -1,4 +0,0 @@
-docker-compose -f docker-compose.test.yaml up --abort-on-container-exit
-EXIT_CODE=$(docker-compose -f docker-compose.test.yaml ps -q app | xargs docker inspect -f '{{ .State.ExitCode }}')
-docker-compose -f docker-compose.test.yaml down
-exit $EXIT_CODE
